84b9d159ae30cbb83ae0694157f7e63ad39ebbc5
Nur Sakinah <nursakinah2402@gmail.com>
2022-10-30 15:17:21 +0700

    first commit

---
+++ src/main/java/pws/takehome1/Takehome1Application.java
@@ -0,0 +1,13 @@
+package pws.takehome1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Takehome1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Takehome1Application.class, args);
+	}
+
+}
