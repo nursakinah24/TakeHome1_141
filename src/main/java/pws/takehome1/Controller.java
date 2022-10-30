a178e375e657398703c77b1a226219920027680a
Nur Sakinah <nursakinah2402@gmail.com>
2022-10-30 16:57:13 +0700

    Adding RequestParam and Model on Controller

--- src/main/java/pws/takehome1/Controller.java
+++ src/main/java/pws/takehome1/Controller.java
@@ -4,10 +4,28 @@
  */
 package pws.takehome1;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  *
  * @author Asus
  */
+
+@org.springframework.stereotype.Controller
 public class Controller {
-    
+    @RequestMapping
+    @ResponseBody
+    public String inputData( @RequestParam(value = "varName") String nameInput,
+            @RequestParam(value = "varLoc") String locInput,
+            @RequestParam(value = "varImage") String imgInput,
+            Model input
+    )
+    {
+        input.addAttribute("name", nameInput);
+        input.addAttribute("loc", locInput);
+        return "viewpage";
+    }
 }
