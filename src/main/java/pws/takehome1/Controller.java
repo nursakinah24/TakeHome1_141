aeeea13d8ccbd740d8fbb0f44e40ff84eb997558
Nur Sakinah <nursakinah2402@gmail.com>
2022-10-30 17:40:02 +0700

    Adding Name and Location for output on viewpage.html

--- src/main/java/pws/takehome1/Controller.java
+++ src/main/java/pws/takehome1/Controller.java
@@ -4,8 +4,17 @@
  */
 package pws.takehome1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
