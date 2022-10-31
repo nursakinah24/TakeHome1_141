e3e560305ca15b339d2713f8615589e24436e1be
Nur Sakinah <nursakinah2402@gmail.com>
2022-10-31 10:48:34 +0700

    adding encode Image to return image to viewpage on controller and fix index

--- src/main/java/pws/takehome1/Controller.java
+++ src/main/java/pws/takehome1/Controller.java
@@ -8,15 +8,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -25,36 +30,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-    @RequestMapping("/mypage")
+    @RequestMapping("/myPage")
     public String inputData( @RequestParam(value = "varName") String nameInput,
             @RequestParam(value = "varLoc") String locInput,
-            @RequestParam(value = "varImage") String imgInput,
+            @RequestParam(value = "varImage") MultipartFile imgInput,
             Model input
-    )
-    {
+    ) throws IOException{ byte[] img = imgInput.getBytes();
+        String encodeImage = Base64.encodeBase64String(img);
+        String imageInput = "data:image/png;base64,".concat(encodeImage);
+        
         input.addAttribute("name", nameInput);
         input.addAttribute("loc", locInput);
-        input.addAttribute("img", imgInput);
-        return "viewpage";
-    }
+        input.addAttribute("img", imageInput);
         
-    @RequestMapping(value = "/image-resource{varImage}", method = RequestMethod.GET)
-    @ResponseBody
-    public ResponseEntity<ByteArrayResource> getImage(@PathVariable("varImage")String photo) throws IOException{
-        if (!photo.equals("")|| photo != null) {
-            try{
-                Path filename = Paths.get("uploads",photo);
-                byte[] buffer = Files.readAllBytes(filename);
-                ByteArrayResource byteArrayResource = new ByteArrayResource(buffer);
-                return ResponseEntity.ok()
-                        .contentLength(buffer.length)
-                        .contentType(MediaType.parseMediaType("image/png"))
-                        .body(byteArrayResource);
-            }catch (Exception e) {
-            }
-            
-        }
-        return ResponseEntity.badRequest().build();
-
+        return "viewpage";
     }
 }
